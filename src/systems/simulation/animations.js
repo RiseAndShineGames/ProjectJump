c3080c85b4234585f2cb421970a8289be6a76598
Anthony Quisenberry <anthony.quisenberry01@gmail.coim>
2016-07-09 13:04:43 -0400

    adding image

--- src/systems/simulation/animations.js
+++ src/systems/simulation/animations.js
@@ -3,33 +3,33 @@
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	game.entities.registerSearch("playerVelocity", ["player", "velocity"]);
     ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
-        var animation = game.entities.get(entity, "animation");
-        var grounded = game.entities.get(entity, "grounded");
-        var velocity = game.entities.get(entity, "velocity");
-        var state = game.entities.get(entity,"adult") ? "adult" : "baby";
-        var isControllable = game.entities.get(entity, "isControllable");
-        if (game.entities.get(entity, "isControllable")) {
-            if (grounded) {
-                if (velocity.x > 0) {
-                    animation.name = state + "-idle-right";
-                } else {
-                    animation.name = state + "-idle-left";
-                }
-                if (game.inputs.button("left") && isControllable) {
-                    animation.name = state + "-walk-left";
-                    game.entities.set(entity, "wasRight", false);
-                } else if (game.inputs.button("right") && isControllable) {
-                    animation.name = state + "-walk-right";
-                    game.entities.set(entity, "wasRight", true);
-                }
-            } else {
-                if (velocity.x > 0) {
-                    animation.name = state + "-jump-right";
-                } else {
-                    animation.name = state + "-jump-left";
-                }
-            }
-        }
+        var image = game.entities.get(entity, "image");
+        // var grounded = game.entities.get(entity, "grounded");
+        // var velocity = game.entities.get(entity, "velocity");
+        // var state = game.entities.get(entity,"adult") ? "adult" : "baby";
+        // var isControllable = game.entities.get(entity, "isControllable");
+        // if (game.entities.get(entity, "isControllable")) {
+        //     if (grounded) {
+        //         if (velocity.x > 0) {
+        //             animation.name = state + "-idle-right";
+        //         } else {
+        //             animation.name = state + "-idle-left";
+        //         }
+        //         if (game.inputs.button("left") && isControllable) {
+        //             animation.name = state + "-walk-left";
+        //             game.entities.set(entity, "wasRight", false);
+        //         } else if (game.inputs.button("right") && isControllable) {
+        //             animation.name = state + "-walk-right";
+        //             game.entities.set(entity, "wasRight", true);
+        //         }
+        //     } else {
+        //         if (velocity.x > 0) {
+        //             animation.name = state + "-jump-right";
+        //         } else {
+        //             animation.name = state + "-jump-left";
+        //         }
+        //     }
+        // }
 
     }, "playerVelocity");
 };
